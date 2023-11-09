53ff3d48ebb5cdef50f981577e70721e2e1df82c
cagancayco <christina.gancayco@gmail.com>
2023-11-09 09:27:36 -0800

    Resetting hp_tuning.py

--- hp_tuning.py
+++ hp_tuning.py
@@ -14,7 +14,7 @@
 
 import tensorflow as tf
 from tensorboard.plugins.hparams import api as hp
-import sys
+
 
 # ### Download the FashionMNIST dataset and scale it:
 
@@ -35,13 +35,6 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([16, 32]))
 HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1, 0.2))
 HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
-
-params = []
-
-for num_units in HP_NUM_UNITS.domain.values:
-  for dropout_rate in (HP_DROPOUT.domain.min_value, HP_DROPOUT.domain.max_value):
-    for optimizer in HP_OPTIMIZER.domain.values:
-        params.append([num_units, dropout_rate, optimizer])
 
 METRIC_ACCURACY = 'accuracy'
 
@@ -92,16 +85,19 @@ def run(run_dir, hparams):
 # In[ ]:
 
 
-session_num = int(sys.argv[1])
+session_num = 0
 
-
-hparams = {
-      HP_NUM_UNITS: params[session_num][0],
-      HP_DROPOUT: params[session_num][1],
-      HP_OPTIMIZER: params[session_num][2],
-}
-run_name = "run-%d" % session_num
-print('--- Starting trial: %s' % run_name)
-print({h.name: hparams[h] for h in hparams})
-run('logs/hparam_tuning/' + run_name, hparams)
+for num_units in HP_NUM_UNITS.domain.values:
+  for dropout_rate in (HP_DROPOUT.domain.min_value, HP_DROPOUT.domain.max_value):
+    for optimizer in HP_OPTIMIZER.domain.values:
+      hparams = {
+          HP_NUM_UNITS: num_units,
+          HP_DROPOUT: dropout_rate,
+          HP_OPTIMIZER: optimizer,
+      }
+      run_name = "run-%d" % session_num
+      print('--- Starting trial: %s' % run_name)
+      print({h.name: hparams[h] for h in hparams})
+      run('logs/hparam_tuning/' + run_name, hparams)
+      session_num += 1
 
